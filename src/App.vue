beee78d034eea47482967b912b5c1d36a18ec3d3
Vladimir-19 <v.s.solovey@gmail.com>
2025-03-02 16:50:37 -0800

    file name updated

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <v-main>
       <HelloWorld />
     </v-main>
+    <CommentsList />
   </v-app>
 </template>
 
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
+import CommentsList from "./components/CommentsList.vue";
 </script>
